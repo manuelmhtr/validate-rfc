ac8ad6a9d59a34b2e7216de3e1c6d369cb707070
Raschid JF Rafaelly <me@raschidjfr.dev>
2021-08-21 11:58:25 -0500

    fix(ts): remove duplicated error definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,5 +2,5 @@ export default function validateRfc<T extends string>(rfc: T, options?: { omitVe
   isValid: boolean,
   type?: 'company' | 'person' | 'foreign' | 'generic',
   rfc?: T,
-  errors?: ('INVALID_FORMAT' | 'INVALID_DATE' | 'INVALID_VERIFICATION_DIGIT' | 'FORBIDDEN_WORD' | 'FORBIDDEN_WORD')[]
+  errors?: ('INVALID_FORMAT' | 'INVALID_DATE' | 'INVALID_VERIFICATION_DIGIT' | 'FORBIDDEN_WORD')[]
 };
